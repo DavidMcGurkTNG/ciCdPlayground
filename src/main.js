f0215d7bb69e97d3071f691e971bc6ec3505be24
Tech Day <techday@Davids-MacBook-Pro.local>
2024-11-15 13:53:49 +0100

    edit info

--- src/main.js
+++ src/main.js
@@ -1,28 +1,24 @@
-import App from './App.svelte';
+import App from "./App.svelte";
 
 new App({
-	target: document.body,
-	props: {
-		// What's your name?
-		name: 'James Butler',
-		// In the following fiels you can either give a single string, 
-		// or an array of bullet points
-		
-		// What do you associate with the term 'CI/CD'?
-		associations: [
-			'Ehm, what?'
-		],
-		// Which CI/CD tools do you use in your project?
-		tools: 'Jenkins, maybe?',
-		// What do you want to learn in this workshop? 
-		expectations: [
-			'what you are talking about'
-		],
-		// What do you like to do when you're not coding?
-		hobbies: [
-			'play chess', 
-			'do skydiving', 
-			'do my taxes'
-		]
-	}
+  target: document.body,
+  props: {
+    // What's your name?
+    name: "David McGurk",
+    // In the following fiels you can either give a single string,
+    // or an array of bullet points
+
+    // What do you associate with the term 'CI/CD'?
+    associations: [
+      "Agile software development, pipelines, software lifecycles",
+    ],
+    // Which CI/CD tools do you use in your project?
+    tools: "Github actions",
+    // What do you want to learn in this workshop?
+    expectations: [
+      "What CI/CD really means in practice, the basics of jenkins and how that compares to github actions",
+    ],
+    // What do you like to do when you're not coding?
+    hobbies: ["play football", "play guitar and bass", "do my taxes :)"],
+  },
 });
